0c3912ab1bead5bb8bd8d9ef45aa807371bf1b34
technosentience <28826901+technosentience@users.noreply.github.com>
2022-05-08 14:48:09 +0400

    Definition and proof improvements.

--- src/simp_groups.lean
+++ src/simp_groups.lean
@@ -8,11 +8,13 @@ open_locale simplicial
 
 universes u
 
-variable n : ℕ
-variable k : fin (n + 3)
+variable {n : ℕ}
+variable {k : fin (n + 3)}
 
 @[derive [large_category]]
 def sGrp : Type (u+1) := simplicial_object Group.{u}
+
+variable {G : sGrp.{u}}
 
 def to_sSet : sGrp.{u} ⥤ sSet.{u} := ((simplicial_object.whiskering _ _).obj (forget Group))
 
@@ -33,53 +35,46 @@ lemma induction_succ
   @fin.induction n C h0 hs i.succ =
     hs i (@fin.induction n C h0 hs i.cast_succ) := by cases i; refl
 
-def u (G : sGrp.{u}) {n : ℕ}
-  {k : fin (n + 3)}
+@[simp]
+lemma reverse_induction_cast_succ'
+  {C : fin (n + 1) → Sort*}
+  (h0 : C (fin.last n))
+  (hs : ∀ i : fin n, C i.succ → C i.cast_succ) (i : fin n):
+  @fin.reverse_induction n C h0 hs i.cast_succ =
+    hs i (@fin.reverse_induction n C h0 hs i.succ) := by apply fin.reverse_induction_cast_succ
+
+def u
   (y : excluded_part n k → G _[n + 1])
   (conn : connected_horn.{u} n k G y)
-  (r : fin (n + 3)) (h : r < k) : G _[n + 2] :=
-  fin.induction (λ h, G.σ 0 (y ⟨0, ne_of_lt h⟩))
-  (λ r' ih h,
-  let u' := ih (lt_trans fin.lt_succ h) in
-  let a' := G.δ r'.succ (u' ⁻¹) in
-  let b' := y ⟨r'.succ, ne_of_lt h⟩ in
-  u' * G.σ r'.succ.cast_pred (a' * b') ) r h
+  (r : fin (n + 3)) (h : r ≤ k) : G _[n + 2] :=
+  fin.induction (λ h, 1) (λ r' ih h,
+    have h' : r'.cast_succ < k := fin.cast_succ_lt_iff_succ_le.mpr h,
+    let u' := ih (le_of_lt h') in
+    u' * G.σ r' (G.δ r'.cast_succ (u' ⁻¹) * y ⟨r'.cast_succ, ne_of_lt h'⟩ ) ) r h
 
 lemma sset_comp (X : sGrp) {i j k} (f : X _[i] ⟶ X _[j])
   (g : X _[j] ⟶ X _[k]) (x : X _[i]) : g (f x) = (f ≫ g) x := rfl
 
-lemma du (G : sGrp.{u}) {n : ℕ}
-  {k : fin (n + 3)}
+lemma du
   (y : excluded_part n k → G _[n + 1])
   (conn : connected_horn.{u} n k G y)
-(r i: fin (n + 3)) (h : r < k) (hi : i ≤ r) : G.δ i (u G y conn r h) = y ⟨i, ne_of_lt (gt_of_gt_of_ge h hi)⟩ :=
+(r i: fin (n + 3)) (h : r ≤ k) (hi : i < r) : G.δ i (u y conn r h) = y ⟨i, ne_of_lt (lt_of_lt_of_le hi h)⟩ :=
 begin
   refine fin.induction _ _ r h i hi;
   clear hi i h r,
   focus {
     intros h i hi,
-    cases i, cases hi,
-    dsimp [u],
-    rw [sset_comp],
-    have h' := @simplicial_object.δ_comp_σ_self _ _ G (n + 1) 0,
-    simp only [fin.cast_succ_zero] at h',
-    rw h',
-    refl
+    cases hi,
   },
   intros r ih h i hi,
-  have h' := @simplicial_object.δ_comp_σ_self _ _ G (n + 1) r.succ.cast_pred,
-  have h'' : fin.cast_succ r.succ.cast_pred = r.succ := by {
-      rw [fin.cast_succ_cast_pred],
-      exact gt_of_ge_of_gt (fin.le_last k) h,
-  },
-  rw h'' at h',
-  have irlv : i ≠ k := ne_of_lt (gt_of_gt_of_ge h hi), 
-  change G.δ i (u G y conn r.succ h) = y {i := i, h := irlv},
-  by_cases hi' : i = r.succ,
+  have h' := @simplicial_object.δ_comp_σ_self _ _ G (n + 1) r,
+  have irlv : i ≠ k := ne_of_lt (lt_of_lt_of_le hi h),
+  change G.δ i (u y conn r.succ h) = y {i := i, h := irlv},
+  by_cases hi' : i = r.cast_succ,
   focus {
     subst hi',
     clear ih,
-    dsimp [u],
+    simp_rw [u],
     rw [induction_succ],
     rw [←u],
     swap, assumption,
@@ -89,59 +84,101 @@ begin
     rw [h'],
     simp only [id_apply, mul_inv_cancel_left],
   },
-  have hi'' : i < r.succ := ne.lt_of_le hi' hi,
+  have hi'' : i < r.cast_succ := ne.lt_of_le hi' (fin.le_cast_succ_iff.mpr hi),
   clear hi hi',
-  have hi : i ≤ r.cast_succ := fin.le_cast_succ_iff.mpr hi'',
-  have ihi := ih (lt_trans fin.lt_succ h) i hi,
-  dsimp [u],
+  have hi := hi'', clear hi'',
+  specialize ih (le_of_lt $ lt_of_lt_of_le fin.lt_succ h) i hi,
+  simp_rw [u],
   rw [induction_succ],
   rw [←u], swap, assumption,
-  simp [ihi],
+  simp only [ih, map_inv, map_mul, mul_right_eq_self],
   set j := i.cast_pred,
-  have hj : j.cast_succ = i := by {
+  have hj : i = j.cast_succ := by {
     rw [fin.cast_succ_cast_pred],
-    exact gt_of_ge_of_gt (fin.le_last r.succ) hi'',
+    exact lt_of_lt_of_le hi (fin.le_last r.cast_succ),
   },
-  rw [←hj],
-  have r_succ_leq_last : r.succ < fin.last (n + 2) := gt_of_ge_of_gt (fin.le_last k) h,
-  have r_leq_last : r < fin.last (n + 1) := by {
-    rw [←fin.succ_last, fin.succ_lt_succ_iff] at r_succ_leq_last,
-    assumption,
-  },
-  have hr : r.succ.cast_pred = r.cast_pred.succ := by {
-    rw [←fin.cast_succ_inj, ←fin.succ_cast_succ, fin.cast_succ_cast_pred, fin.cast_succ_cast_pred],
-    swap,
-    assumption,
-    assumption,
-  },
-  rw [hr],
+  rw [hj],
+  set r' := r.pred (by {
+    intro h0,
+    rw h0 at hi,
+    cases hi,
+  }),
+  have hr : r = r'.succ := by rw [fin.succ_pred],
+  simp_rw [hr],
   nth_rewrite 0 [sset_comp],
   rw [simplicial_object.δ_comp_σ_of_le], swap,
   rw [fin.le_cast_succ_iff, ←fin.cast_succ_lt_cast_succ_iff],
-  rw [hj, ←hr],
-  rwa [fin.cast_succ_cast_pred],
-  exact gt_of_ge_of_gt (fin.le_last k) h,
+  rwa [←hj, ←hr],
   rw [←sset_comp],
   nth_rewrite 1 [sset_comp],
-  have j_leq_r : j ≤ r := by {
-    rw le_iff_lt_or_eq,
-    rw [←hj] at hi,
-    cases lt_or_eq_of_le hi,
-    left, rwa [←fin.cast_succ_lt_cast_succ_iff],
-    right, rwa [←fin.cast_succ_inj],
-  },
+  simp_rw [←fin.succ_cast_succ],
+  have j_leq_r' : j ≤ r'.cast_succ :=
+    by rwa [fin.le_cast_succ_iff, ←hr, ←fin.cast_succ_lt_cast_succ_iff, ←hj],
   rw [simplicial_object.δ_comp_δ], swap, assumption,
-  rw [←sset_comp, hj, ihi],
+  simp_rw [←sset_comp, ←hj, fin.succ_cast_succ, ←hr, ih],
   nth_rewrite 1 [sset_comp],
-  simp_rw [←hj],
-  rw [simplicial_object.δ_comp_σ_of_le], swap, rwa [fin.cast_succ_cast_pred], assumption,
+  simp_rw [hj, hr],
+  rw [simplicial_object.δ_comp_σ_of_le], swap, assumption,
   rw [←sset_comp, ←map_inv, ←map_mul],
-  have j_neq_k : j.cast_succ ≠ k := by rwa hj,
-  have hc := conn ⟨j, r, j_leq_r, j_neq_k, ne_of_lt h⟩, simp only at hc,
-  change simplicial_object.δ G j (y {i := r.succ, h := _}) = simplicial_object.δ G r (y {i := j.cast_succ, h := _}) at hc,
+  have j_neq_k : j.cast_succ ≠ k := by rwa [←hj],
+  have r_lt_k : r'.cast_succ.succ < k :=
+    by rwa [fin.succ_cast_succ, ←hr, fin.cast_succ_lt_iff_succ_le],
+  have hc := conn ⟨j, r'.cast_succ, j_leq_r', j_neq_k, ne_of_lt r_lt_k⟩, simp only at hc,
+  change simplicial_object.δ G j (y {i := r'.cast_succ.succ, h := _}) = simplicial_object.δ G r'.cast_succ (y {i := j.cast_succ, h := _}) at hc,
+  simp_rw [fin.succ_cast_succ] at hc,
   rw hc,
   simp only [mul_left_inv, map_one],
 end
+
+def u'
+  (y : excluded_part n k → G _[n + 1])
+  (conn : connected_horn.{u} n k G y) : G _[n + 2] := u y conn k (by refl)
+
+def du'
+  (y : excluded_part n k → G _[n + 1])
+  (conn : connected_horn.{u} n k G y)
+  (i : fin (n + 3)) (h : i < k) : G.δ i (u' y conn) = y ⟨i, ne_of_lt h⟩ :=
+begin
+  dsimp [u'],
+  apply du,
+  assumption,
+end
+
+-- def w
+--   (y : excluded_part n k → G _[n + 1])
+--   (conn : connected_horn.{u} n k G y)
+--   (r : fin (n + 3)) (h : r > k) : G _[n + 2] :=
+--     fin.reverse_induction (λ _, u' y conn) (λ r' ih h,
+--       let w' := ih sorry in
+--       let a' := G.δ r'.succ.cast_pred.cast_pred (w' ⁻¹) in
+--       let b' := y ⟨r'.succ, sorry⟩ in
+--       w' * G.σ r.cast_pred (a' * b')
+--     ) r h
+
+-- lemma dw
+--   (y : excluded_part n k → G _[n + 1])
+--   (conn : connected_horn.{u} n k G y)
+--   (r i : fin (n + 3)) (h : r > k) (hi : i < k ∨ i ≥ r) :
+--   G.δ i (w y conn r h) = y ⟨i, sorry⟩ :=
+-- begin
+--   refine fin.reverse_induction _ _ r h i hi;
+--   clear' r h i hi,
+--   focus {
+--     intros h i ih,
+--     cases ih, swap, sorry,
+--     dsimp [w],
+--     rw [fin.reverse_induction_last],
+--     apply du', assumption,
+--   },
+--   intros r ih h i hi,
+--   by_cases hi' : i = r.succ,
+--   focus {
+--     subst hi',
+--     clear ih hi,
+--     simp_rw [w], rw [reverse_induction_cast_succ'],
+--     -- change G.δ r.succ (w y conn r.succ _) * G.σ r.cast_pred (G.δ r'.succ.cast_pred.cast_pred (w y conn r.succ))
+--   }
+-- end
 
 -- def sGrp_Ext (G : sGrp.{u}) : has_matching_faces.{u} G :=
 -- begin
